8e3fac5f4b7d68967fc3c6c569c0d07ce13e28ff
nikiforov.ds2190 <nikiforov.ds2190@M410C2-R55>
2023-05-11 12:29:01 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Person
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+}
+
+public class Address
+{
+	public string Street { get; set; }
+	public string City { get; set; }
+	public string ZipCode { get; set; }
+}
+
+public class Company
+{
+	public string Name { get; set; }
+	public Address Address { get; set; }
+}
+
+public class Employee : Person
+{
+	public decimal Salary { get; set; }
+	public Company Company { get; set; }
+}
+public class Person1
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+}
+
+public class Address1
+{
+	public string Street { get; set; }
+	public string City { get; set; }
+	public string ZipCode { get; set; }
+}
+
+public class Company1
+{
+	public string Name { get; set; }
+	public Address Address { get; set; }
+}
+
+public class Employee1 : Person
+{
+	public decimal Salary { get; set; }
+	public Company Company { get; set; }
+}
